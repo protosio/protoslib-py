acc9e789cd1e39202bae79c37d8505fbd2e8933a
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-03-09 19:49:35 +0200

    get domain and create resource are implemented atm

--- protos.py
+++ protos.py
@@ -1,15 +1,32 @@
-import requests
+from requests import Request, Session
+from . import exceptions
 
 class Protos(object):
     """
     Implements a Protos client for the internal API
     """
 
-    def __init__(self, appid, url='protos'):
+    def __init__(self, appid, url='http://protos:8080/'):
         self.appid = appid
         self.url = url
 
+    def _send_request(self, req):
+        req.headers = {'Appid': self.appid}
+        req.url = self.url + req.url
+        pre = req.prepare()
+        s = Session()
+        resp = s.send(pre)
+        if resp.status_code != 200:
+            raise exceptions.ProtosException(resp.text.rstrip())
+        return resp
+
     def get_domain(self):
-        r = requests.get(self.url + 'internal/info/domain')
+        req = Request('GET', 'internal/info/domain')
+        r = self._send_request(req)
         info = r.json()
         return info['Domain']
+
+    def create_resource(self, resource):
+        req = Request('POST', 'internal/resources', json=resource)
+        r = self._make_request(req)
+        return r.json()
--- util.py
+++ util.py
@@ -1,10 +1,8 @@
 import os
-
-class ProtosException(Exception):
-    pass
+from . import exceptions
 
 def get_app_id():
     appid = os.environ.get('APPID')
     if not appid:
-        raise ProtosException('The APPID environment variable is empty or does not exist')
+        raise exceptions.ProtosException('The APPID environment variable is empty or does not exist')
     return appid
