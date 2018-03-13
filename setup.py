c354708fae7a5c0727bc19fd33ca0311c84323be
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-03-13 16:50:01 +0200

    added a setup file for the package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup_info(name='protoslib',
+      version='0.1',
+      description='Python library for interacting with the internal Protos API',
+      author='Alex Giurgiu',
+      author_email='alex@giurgiu.io',
+      url='https://github.com/nustiueudinastea/protoslib-py',
+      packages=['protoslib'],
+     )
+
+setup(**setup_info)
