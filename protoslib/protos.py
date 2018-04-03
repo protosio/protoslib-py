177a0297a7998f06543c0d322cb7328ed7c1b1d6
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-04-03 15:06:23 +0300

    the user authentication call now returns a payload with information about the user

--- protoslib/protos.py
+++ protoslib/protos.py
@@ -63,8 +63,9 @@ class Protos(object):
     def authenticate_user(self, username, password):
         userdata = {'username': username, 'password': password}
         req = Request('POST', 'internal/user/auth', json=userdata)
+        user = None
         try:
-            self._send_request(req)
+            user = self._send_request(req)
         except exceptions.Unauthorized:
-            return False
-        return True
+            return user
+        return user
